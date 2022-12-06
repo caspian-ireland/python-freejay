e56e144c34380038b20a1c5cac6754078ed28626
Caspian Ireland <59339201+caspian-ireland@users.noreply.github.com>
2022-12-06 17:20:19 +0000

    feat(djplayer)!: depends on player
    djplayer now depends on player interface rather than mpv.

--- freejay/djplayer.py
+++ freejay/djplayer.py
@@ -4,16 +4,10 @@ The DJPlayer module holds the DJPlayer interface and implementations, representi
 audio player.
 """
 
-import os
-import errno
 import logging
-import functools
-import time
-import typing
 import abc
-from mpv import MPV
+from freejay.player import Player
 
-TCallable = typing.TypeVar("TCallable", bound=typing.Callable)
 logger = logging.getLogger(__name__)
 
 
@@ -24,11 +18,11 @@ class DJPlayer(abc.ABC):
     """DJ media player interface."""
 
     @abc.abstractmethod
-    def __init__(self, player: MPV):
+    def __init__(self, player: Player):
         """Construct DJPlayer.
 
         Args:
-            player(MPV): MPV media player.
+            player(Player): Audio player.
 
         """
         pass
@@ -113,70 +107,7 @@ class DJPlayer(abc.ABC):
         pass
 
 
-"""MPV Implementation"""
-
-
-class MPVLoadError(Exception):
-    """
-    Exception raised for MPV load error. Likely due to incompatible file format.
-    """
-
-    def __init__(self, filename: str, message: str = "MPV Could not load file."):
-        """Construct MPVLoadError.
-
-        Args:
-            filename (str): The file that caused a load error
-            message (str, optional): Exception message.
-                Defaults to 'MPV Could not load file.'.
-        """
-        self.filename = filename
-        super().__init__(message)
-
-
-def _check_file_loaded(func: TCallable) -> TCallable:
-    """Check file is loaded before running DJPlayer methods.
-
-    Args:
-        func (TCallable): DJPlayer method
-
-    Returns:
-        TCallable: DJPlayer method wrapped with conditional statement.
-    """
-
-    @functools.wraps(func)
-    def wrapper_check_file_loaded(self, *args, **kwargs):
-        if self.filename:
-            return func(self, *args, **kwargs)
-
-        logger.info("File not loaded.")
-        return None
-
-    return typing.cast(TCallable, wrapper_check_file_loaded)
-
-
-def mpv_log_handler(loglevel: str, component: str, message: str) -> None:
-    """Convert mpv log events into log events.
-
-    Args:
-        loglevel (str): Log level. Currently supported levels are: 'trace', 'debug',
-            'warn', 'error', 'fatal'. All others will be raised as `logging.ERROR`.
-        component (str): mpv component raising log event
-        message (str): Log message
-    """
-    mpv_log_levels = {
-        "fatal": logging.FATAL,
-        "error": logging.ERROR,
-        "warn": logging.WARN,
-        "info": logging.INFO,
-        "debug": logging.DEBUG,
-        "trace": logging.DEBUG,
-    }
-    logger.log(
-        mpv_log_levels.get(loglevel, logging.ERROR), "MPV-%s: %s", component, message
-    )
-
-
-class DJPlayerMpv(DJPlayer):
+class DJPlayerBasic(DJPlayer):
     """DJ Media player powered by MPV.
 
     Attributes:
@@ -184,7 +115,6 @@ class DJPlayerMpv(DJPlayer):
         filename (str): Audio file loaded in player.
         __filename(str): Audio file loaded in player.
         __speed(float): Playback speed.
-        __playing(bool): Is audio currently playing?
         __cue_mode (bool): Is the player in cue mode?
         __time_cue (float): The cue point.
         __player(MPV): Media player.
@@ -200,16 +130,15 @@ class DJPlayerMpv(DJPlayer):
         jog(amount): Jog (relative seek) the track by amount.
     """
 
-    def __init__(self, player: MPV):
+    def __init__(self, player: Player):
         """
         Construct DJPlayerMpv.
 
         Args:
-            player(MPV): MPV media player.
+            player(Player): Media player.
         """
         self.__filename = ""
         self.__speed = 1.0
-        self.__playing = False
         self.__cue_mode = True
         self.__time_cue = 0.0
         self.__player = player
@@ -226,62 +155,32 @@ class DJPlayerMpv(DJPlayer):
                 media player. Likely due to incompatible file format.
         """
         # If the file is playing, do nothing and log a warning.
-        if self.__playing:
+        if self.__player.playing:
             logger.warning("Track is still playing!")
         else:
-
-            if not os.path.exists(filename):
-                logger.error("File %s could not be found.", filename)
-                raise FileNotFoundError(
-                    errno.ENOENT, os.strerror(errno.ENOENT), filename
-                )
-
-            self.__pause()
             self.__cue_mode = True
-            self.__player.play(filename=filename)
-            time.sleep(0.1)
+            self.__player.load(filename=filename)
+            self.__filename = filename
+            self.__time_cue = self.__player.time_start
 
-            # Dirty workaround to check if file succesfully loaded.
-            # See issue https://github.com/jaseg/python-mpv/issues/194
-            if self.__player.path:
-                logger.info("File %s loaded.", filename)
-                self.__filename = filename
-                self.__time_cue = self.__player.time_start
-            else:
-                self.__filename = ""
-                self.__time_cue = 0.0
-                logger.error("File %s could not be loaded.", filename)
-                raise MPVLoadError(filename)
-
-    @_check_file_loaded
     def play_pause(self):
         """Play or pause the track."""
-        if not self.__playing:
-            self.__play()
+        if not self.__player.playing:
+            self.__player.play()
         # If in cue mode and playing then user is cueing the track
         # and playback should continue
         else:
             if not self.__cue_mode:
-                self.__pause()
+                self.__player.pause()
 
         self.__cue_mode = False
 
-    def __play(self):
-        self.__player.pause = False
-        self.__playing = True
-
-    def __pause(self):
-        self.__player.pause = True
-        self.__playing = False
-
-    @_check_file_loaded
     def stop(self):
         """Stop playback and return to start of track."""
-        self.__pause()
+        self.__player.pause()
         self.__cue_mode = True
         self.__player.seek(self.__player.time_start, reference="absolute")
 
-    @_check_file_loaded
     def cue_press(self):
         """Imitates cue button press (see method `cue_release` for release)."""
         # Cue behaviour uses the `__cue_mode` attribute to track whether the
@@ -291,21 +190,20 @@ class DJPlayerMpv(DJPlayer):
         # start the track playing. When not in cue mode, `cue_press` will set the
         # cue point and pause the track. This mimics mainstream DJ software behaviour.
         # Note if track is playing, `cue_press` will not set the cue point.
-        if not self.__playing:
+        if not self.__player.playing:
             self.__time_cue = self.__player.time_pos
 
         if self.__cue_mode:
-            self.__play()
+            self.__player.play()
         else:
-            self.__pause()
+            self.__player.pause()
 
         self.__cue_mode = True
 
-    @_check_file_loaded
     def cue_release(self):
         """Imitates cue button release (see method `cue_press` for press)."""
         if self.__cue_mode:
-            self.__pause()
+            self.__player.pause()
             self.__player.seek(amount=self.__time_cue, reference="absolute")
 
     @property
@@ -340,7 +238,7 @@ class DJPlayerMpv(DJPlayer):
         """
         if not -1 < amount < 1:
             raise ValueError("Amount must be in the range (-1, 1).")
-        self.__player.speed = self.__player.speed * (1 + amount)  # type: ignore
+        self.__player.speed = self.__player.speed * (1 + amount)
 
     def nudge_release(self):
         """Pitch nudge stop. See also `nudge_press`."""
