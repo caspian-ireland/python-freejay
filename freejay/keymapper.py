e62dacb1095dab8912805b5c5e0241f1296ba306
Caspian Ireland <59339201+caspian-ireland@users.noreply.github.com>
2023-01-16 16:10:40 +0000

    docs(keymapper): add basic docstrings

--- freejay/keymapper.py
+++ freejay/keymapper.py
@@ -1,6 +1,11 @@
+"""
+Keymapper implements keybindings, mapping key event messages into action messages.
+"""
+
 from freejay import messages as mes
 from freejay import produce_consume as prodcon
 
+# TODO: Better way of storing this
 keybindings = {
     "q": {
         "name": "cue-left",
@@ -22,15 +27,33 @@ keybindings = {
     },
 }
 
-# KeyMapper listens to debouncer/router
-# KeyMapper sends messages to router/event handler
-
 
 class KeyMapper(prodcon.Consumer, prodcon.Producer):
-    def __init__(self, keybindings):
+    """
+    Map key events using keybindings.
+
+    KeyMapper listens for key messages and
+    uses keybindings to translate into relevant
+    messages that can be sent to another consumer.
+    """
+
+    def __init__(self, keybindings: dict):
+        """Construct KeyMapper.
+
+        Args:
+            keybindings (dict): keybindings
+        """
         self.keybindings = keybindings
 
     def map(self, message: mes.Message):
+        """Map key message using keybindings.
+
+        Args:
+            message (mes.Message): Message with content type 'Key' to be mapped.
+
+        Raises:
+            TypeError: Message with type != 'Key'.
+        """
         if isinstance(message.content, mes.Key):
             keybinding = self.keybindings[message.content.sym]
         else:
@@ -47,4 +70,11 @@ class KeyMapper(prodcon.Consumer, prodcon.Producer):
         self.send_message(newmessage)
 
     def on_message_recieved(self, message: mes.Message):
+        """Call `map` method.
+
+        Implement consumer protocol. On message recieved call `map` method.
+
+        Args:
+            message (mes.Message): Message with content type 'Key' to be mapped.
+        """
         self.map(message)
