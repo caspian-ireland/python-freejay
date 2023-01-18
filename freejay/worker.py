b18642c11c1194406af7db425f07d260e5c09f28
Caspian Ireland <59339201+caspian-ireland@users.noreply.github.com>
2023-01-18 10:35:08 +0000

    feat: update worker to use generic handler

--- freejay/worker.py
+++ freejay/worker.py
@@ -1,27 +1,58 @@
 import queue
+import typing
+from freejay import handler
 from freejay import messages as mes
 
 
 # credit https://maldus512.medium.com/how-to-setup-correctly-an-application-with-python-and-tkinter-107c6bc5a45
 
 
-def worker(player, q):
-    message: mes.Message = None
-    while True:
-        try:
-            message = q.get(timeout=0.1)
+class Worker:
+    def init(self, q: queue.Queue[typing.Type[mes.Message]], handler: handler.Handler):
+        self.q = q
+        self.handler = handler
 
-            if message.content. == events.Action.PlayPause:
-                if event.payload.press_release == events.PressRelease.Press:
-                    player.play_pause()
-            if event.payload.action == events.Action.Cue:
-                if event.payload.press_release == events.PressRelease.Press:
-                    player.cue_press()
-                elif event.payload.press_release == events.PressRelease.Release:
-                    player.cue_release()
+    def process(self):
+        while True:
+            try:
+                message = self.q.get(timeout=0.1)
+                self.handler(message)
 
-        except queue.Empty:
-            pass
+            except queue.Empty:
+                pass
 
 
+### Manual Testing ###
 
+
+# class ExampleClass:
+#     def __init__(self):
+#         self.a = "Something Cool"
+
+#     def somemethod(self):
+#         print(self.a)
+
+
+# msg = mes.Message(
+#     sender=mes.Sender(source=mes.Source.PLAYER, trigger=mes.Trigger.BUTTON_PRESS),
+#     content=mes.Button(
+#         press_release=mes.PressRelease.PRESS,
+#         component=mes.Component.LEFT_DECK,
+#         element=mes.Element.CUE,
+#     ),
+#     type=mes.Type.KEY,
+# )
+
+
+# myhandler = Handler()
+
+# exampleclass = ExampleClass()
+# button_cb = make_button_cb(exampleclass.somemethod)
+
+# myhandler.register_handler(
+#     callback=button_cb,
+#     component=mes.Component.LEFT_DECK,
+#     element=mes.Element.CUE,
+# )
+
+# myhandler(msg)
